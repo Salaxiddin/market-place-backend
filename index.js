db16820609cc8e476cc4425cfa3fa958e8d6b6cc
Nesar Ahmed Naeem <hello@nesaran.com>
2023-04-25 03:22:58 +0600

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
+const mobileSchema = require("./schemas/mobileSchema");
 const Mobile = require("./models/Mobile");
 // Create Express app
 const app = express();
@@ -16,14 +16,9 @@ dotenv.config();
 //MiddleWares
 app.use(express.json());
 app.use(cors());
-
-// Routes Schema, Models
+// Routes
 const mobileRoute = require("./routes/mobile.route");
-const smartWatchesRoute = require("./routes/smartWatches.route");
 const versionRoute = require("./routes/versions.route");
-const mobileSchema = require("./schemas/mobileSchema");
-const smartWatchesSchema = require("./schemas/smartWatchesSchema");
-const SmartWatches = require("./models/SmartWatches");
 // Connect to MongoDB database
 mongoose
   .connect(process.env.DB_CONNECT, {
@@ -33,13 +28,12 @@ mongoose
   .then(() => console.log("Connected to database"))
   .catch((err) => console.error("Failed to connect to database", err));
 
-// Define schema
+// Define bus route schema
 mobileSchema;
-smartWatchesSchema;
 
-// Define model
+// Define bus route model
 Mobile;
-SmartWatches;
+
 // Route for index
 app.get("/", (req, res) => {
   res.send("Server is running");
@@ -47,7 +41,6 @@ app.get("/", (req, res) => {
 
 // Define endpoint for getting all bus routes
 app.use("/api/v1/mobile", mobileRoute);
-app.use("/api/v1/smartWatches", smartWatchesRoute);
 app.use("/api/v1/version", versionRoute);
 
 // Start server
