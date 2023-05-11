e4542694f17b9b8c59af5d67fab5d06bdde10275
Nesar Ahmed Naeem <hello@nesaran.com>
2023-05-11 19:22:32 +0600

    updated

--- controllers/gadget.controller.js
+++ controllers/gadget.controller.js
@@ -75,32 +75,30 @@ const getGadgetByBrand = async (req, res) => {
   const totalCount = await Gadget.countDocuments(filter);
 
   const data = await Gadget.find(filter)
-    .sort({ "specifications.LaunchAnnouncement": -1, _id: -1 })
+    .sort({ _id: -1 })
     .skip(skip)
-    .limit(limit)
-    .lean()
-    .then((docs) => {
-      return docs.map((doc) => {
-        let launchAnnouncement = doc.specifications[0].LaunchAnnouncement;
-        if (!launchAnnouncement) {
-          doc.specifications[0].LaunchAnnouncement = null;
-          return doc;
-        }
-        // check if launchAnnouncement is in "yyyy, MMMM" or "yyyy" format
-        if (launchAnnouncement.match(/^\d{4}(, \w+)?$/)) {
-          launchAnnouncement = launchAnnouncement.replace(", ", " ");
-          const date = new Date(launchAnnouncement);
-          if (isNaN(date.getTime())) {
-            doc.specifications[0].LaunchAnnouncement = null;
-          } else {
-            doc.specifications[0].LaunchAnnouncement = date;
-          }
-        } else {
-          doc.specifications[0].LaunchAnnouncement = null;
-        }
-        return doc;
-      });
-    });
+    .limit(limit);
+  //   return docs.map((doc) => {
+  //     let launchAnnouncement = doc.specifications[0].LaunchAnnouncement;
+  //     if (!launchAnnouncement) {
+  //       doc.specifications[0].LaunchAnnouncement = null;
+  //       return doc;
+  //     }
+  //     // check if launchAnnouncement is in "yyyy, MMMM" or "yyyy" format
+  //     if (launchAnnouncement.match(/^\d{4}(, \w+)?$/)) {
+  //       launchAnnouncement = launchAnnouncement.replace(", ", " ");
+  //       const date = new Date(launchAnnouncement);
+  //       if (isNaN(date.getTime())) {
+  //         doc.specifications[0].LaunchAnnouncement = null;
+  //       } else {
+  //         doc.specifications[0].LaunchAnnouncement = date;
+  //       }
+  //     } else {
+  //       doc.specifications[0].LaunchAnnouncement = null;
+  //     }
+  //     return doc;
+  //   });
+  // });
 
   // Calculate total number of pages
   const totalPages = Math.ceil(totalCount / limit);
