2b6d9b402697cfa5bfbd21618481dc7abea57421
Nesar Ahmed Naeem <hello@nesaran.com>
2023-05-19 19:06:30 +0600

    updated

--- routes/gadget.route.js
+++ routes/gadget.route.js
@@ -4,9 +4,9 @@ const gadgetController = require("../controllers/gadget.controller");
 const whiteListedDomain = require("../middleware/whiteListedDomain");
 
 router.get("/", gadgetController.getGadgetByBrand);
-router.get("/details", gadgetController.getGadgetDetails);
-router.get("/brands", gadgetController.getAllGadgetBrands);
-router.get("/search", gadgetController.searchGadgetByTitle);
+router.get("/details", whiteListedDomain, gadgetController.getGadgetDetails);
+router.get("/brands", whiteListedDomain, gadgetController.getAllGadgetBrands);
+router.get("/search", whiteListedDomain, gadgetController.searchGadgetByTitle);
 router.get(
   "/category",
   whiteListedDomain,
